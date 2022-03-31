37913080816a5e2a19237ba8fed9d9155558165c
sahilsharma884 <sahiltinky21@hotmail.com>
2022-03-31 13:46:20 +0530

    31 March 2022

---
+++ Hackerearth/the_cheapest_palindrome.py
@@ -0,0 +1,37 @@
+def checkPalindrome(S, mid):
+    fS = S[:mid]
+    sS = S[-1:-(mid + 1):-1]
+
+    return fS == sS
+
+
+if __name__ == '__main__':
+    T = int(input())
+
+    for _ in range(T):
+        S = input()
+        aCost = int(input())
+        bCost = int(input())
+
+        Cost_list = {'a': aCost, 'b': bCost}
+        mid = int(len(S) / 2)
+        length_S = len(S)
+        cost = 0
+        result_S = ''
+        for i in range(len(S)):
+            if S[i] == '/':
+                v_i = length_S - i - 1
+                if S[v_i] != '/':
+                    result_S += S[v_i]
+                    cost += Cost_list.get(S[v_i])
+                else:
+                    minCost = sorted(Cost_list.items(), key=lambda x: x[1])[0]
+                    result_S += minCost[0]
+                    cost += minCost[1]
+            else:
+                result_S += S[i]
+
+        if checkPalindrome(result_S, mid):
+            print(cost)
+        else:
+            print(-1)
