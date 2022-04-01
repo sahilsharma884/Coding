a33f5aca30f9994b8f3e1ca98ad98d115b584c1a
sahilsharma884 <sahiltinky21@hotmail.com>
2022-04-01 16:02:37 +0530

    1 April 2022

---
+++ Hackerearth/CodeMonk/Monk_and_Rotation.py
@@ -0,0 +1,12 @@
+# Problem Link
+# https://www.hackerearth.com/problem/algorithm/monk-and-rotation-3-bcf1aefe/
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        N, K = list(map(int, input().split(' ')))
+        if K > N:
+            K = K % N
+        A = list(map(int, input().split(' ')))
+        A = A[:N]
+        print(' '.join(list(map(str, A[N - K:])) + list(map(str, A[:N - K]))))
