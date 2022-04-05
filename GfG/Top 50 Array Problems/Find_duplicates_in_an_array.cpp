3a59d96398895df19bd85529cc1eb42763f2e22c
SAHIL <sahiltinky21@hotmail.com>
2022-04-05 15:21:25 +0530

    05 April 2022

--- GfG/Top 50 Array Problems/Find_duplicates_in_an_array.cpp
+++ GfG/Top 50 Array Problems/Find_duplicates_in_an_array.cpp
@@ -47,38 +47,29 @@ class Solution{
   public:
     vector<int> duplicates(int arr[], int n) {
         // code here
-        unordered_map<int, int> dup_list; // take O(1)
         vector<int> result;
-        int i, count = 0;
+        int i;
         
         for(i=0; i<n; i++)
         {
-            auto itr = dup_list.find(arr[i]);
-            if(itr == dup_list.end())
+            arr[arr[i]%n] += n;
+        }
+        
+        
+        for(i=0; i<n; i++)
+        {
+            if((arr[i]/n)>1)
             {
-                dup_list.insert({arr[i],1});
-                count += 1;
-            }
-            else
-            {
-                itr->second += 1;
+                result.push_back(i);
             }
         }
         
-        if(count == n)
+        if(result.size() == 0)
         {
             return {-1};
         }
         else
         {
-            for(auto itr=dup_list.begin(); itr!=dup_list.end(); itr++)
-            {
-                if(itr->second > 1)
-                {
-                    result.push_back(itr->first);
-                }
-            }
-            sort(result.begin(), result.end());
             return result;
         }
     }
